04dff486fe5376ba645c2b3bbc7624d8355037aa
SMPhuc <sonminhphuc41@gmail.com>
2023-06-06 01:00:32 +0700

    update download button

--- assets/js/live-resume.js
+++ assets/js/live-resume.js
@@ -75,7 +75,7 @@ function shareWeb() {
 
 function downloadCV() {
     // Đường dẫn đến tệp CV của bạn
-    var cvURL = 'đường_dẫn_đến_tệp_CV_của_bạn';
+    var cvURL = '/assets/images/CV_SMPhuc.pdf';
 
     // Tạo một phần tử a ẩn
     var link = document.createElement('a');
